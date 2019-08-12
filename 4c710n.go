061da3cf0e59cc588172d5e8e168799632033892
ccamel <camel.christophe@gmail.com>
2019-08-12 11:33:01 +0200

    :bulb: improve documentation

--- 4c710n.go
+++ 4c710n.go
@@ -6,15 +6,20 @@ import (
 	"github.com/rs/zerolog"
 )
 
+// ActionCore specifies the common fields of all kind of actions.
 type ActionCore struct {
-	URI     string `yaml:"uri" validate:"nonzero,min=7"`
-	Timeout int64  `yaml:"timeout"` // Timeout specifies a time limit (in ms) for HTTP requests made.
+	// URI allows to refer to an endpoint.
+	URI string `yaml:"uri" validate:"nonzero,min=7"`
+	// Timeout specifies a time limit (in ms) for HTTP requests made.
+	Timeout int64 `yaml:"timeout"`
 }
 
+// GetURI returns the current configured URI.
 func (a ActionCore) GetURI() string {
 	return a.URI
 }
 
+// Action specifies the behaviour of an action.
 type Action interface {
 	zerolog.LogObjectMarshaler
 	GetURI() string
@@ -22,4 +27,5 @@ type Action interface {
 	DoAction(ctx context.Context) (interface{}, error)
 }
 
+// ActionFactory denotes functions able to return new instances of actions.
 type ActionFactory func() Action
