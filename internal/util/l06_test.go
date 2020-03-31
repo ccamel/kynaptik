d8a119a5397a688a5690a680b3fc8e164ec7fa02
Chris Camel <camel.christophe@gmail.com>
2020-03-31 11:01:21 +0200

    :white_check_mark: add TestResultToLogObjectMarshaller unit test

--- internal/util/l06_test.go
+++ internal/util/l06_test.go
@@ -5,6 +5,7 @@ import (
 
 	"github.com/rs/zerolog"
 	. "github.com/smartystreets/goconvey/convey"
+	"github.com/tcnksm/go-httpstat"
 )
 
 func TestLoggerFunc(t *testing.T) {
@@ -24,6 +25,21 @@ func TestLoggerFunc(t *testing.T) {
 				logger.MarshalZerologObject(e)
 
 				So(called, ShouldBeTrue) // we can't do much more
+			})
+		})
+	})
+}
+
+func TestResultToLogObjectMarshaller(t *testing.T) {
+	Convey("Considering the ResultToLogObjectMarshaller", t, func(c C) {
+		Convey("When calling function", func(c C) {
+			logger := ResultToLogObjectMarshaller(&httpstat.Result{})
+
+			Convey("Then result shall be a marshallable Zerolog object", func(c C) {
+				So(logger, ShouldNotBeNil)
+
+				e := zerolog.Dict()
+				logger.MarshalZerologObject(e) // we can't do much more, just check it doesn't panic
 			})
 		})
 	})
