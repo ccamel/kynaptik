3cfec93a5687839b30c4d4400b734434039209fd
Chris Camel <camel.christophe@gmail.com>
2020-03-31 11:02:00 +0200

    :white_check_mark: add TestHTTPHeaderToLogObjectMarshaller unit test

--- internal/util/l06_test.go
+++ internal/util/l06_test.go
@@ -1,6 +1,7 @@
 package util
 
 import (
+	"net/http"
 	"testing"
 
 	"github.com/rs/zerolog"
@@ -34,6 +35,23 @@ func TestResultToLogObjectMarshaller(t *testing.T) {
 	Convey("Considering the ResultToLogObjectMarshaller", t, func(c C) {
 		Convey("When calling function", func(c C) {
 			logger := ResultToLogObjectMarshaller(&httpstat.Result{})
+
+			Convey("Then result shall be a marshallable Zerolog object", func(c C) {
+				So(logger, ShouldNotBeNil)
+
+				e := zerolog.Dict()
+				logger.MarshalZerologObject(e) // we can't do much more, just check it doesn't panic
+			})
+		})
+	})
+}
+
+func TestHTTPHeaderToLogObjectMarshaller(t *testing.T) {
+	Convey("Considering the HTTPHeaderToLogObjectMarshaller", t, func(c C) {
+		Convey("When calling function", func(c C) {
+			logger := HTTPHeaderToLogObjectMarshaller(http.Header{
+				"foo": []string{"bar"},
+			})
 
 			Convey("Then result shall be a marshallable Zerolog object", func(c C) {
 				So(logger, ShouldNotBeNil)
